61520e887135263201ad6c87b075105e4244e25d
t-kusakabe <ttmac0704@gmail.com>
2019-11-19 14:30:00 +0900

    ````

--- bin/sample-cdk-ec2.ts
+++ bin/sample-cdk-ec2.ts
@@ -4,4 +4,9 @@ import cdk = require('@aws-cdk/core');
 import { SampleCdkEc2Stack } from '../lib/sample-cdk-ec2-stack';
 
 const app = new cdk.App();
-new SampleCdkEc2Stack(app, 'SampleCdkEc2Stack');
+new SampleCdkEc2Stack(app, 'SampleCdkEc2Stack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
